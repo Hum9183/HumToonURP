fe175a0a7f74c4bd1de80de57b1d4f2d45c99152
Hum9183 <hum9183@gmail.com>
2023-08-20 17:09:46 +0900

    GUIAdvancedOptions.csを追加

---
+++ Editor/GUIAdvancedOptions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Rendering;
+using UnityEditor.Rendering.Universal;
+using UnityEditor.ShaderGraph;
+using UnityEditor.ShaderGraph.Drawing;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+// using static Unity.Rendering.Universal.ShaderUtils;
+using RenderQueue = UnityEngine.Rendering.RenderQueue;
+
+namespace HumToon.Editor
+{
+    internal partial class HumToonGUI
+    {
+        private void DrawAdvancedOptions(Material material)
+        {
+            _materialEditor.IntSliderShaderProperty(_matProps.QueueOffset, -QueueOffsetRange, QueueOffsetRange, Styles.queueSlider);
+            _materialEditor.EnableInstancingField();
+        }
+    }
+}
