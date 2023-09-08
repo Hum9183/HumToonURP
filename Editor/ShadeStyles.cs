590f5bccd74649423d1ca5f58cd10831e7da4e04
Hum9183 <hum9183@gmail.com>
2023-09-08 21:53:25 +0900

    Shadeのプロパティにtooltipを追加

--- Editor/ShadeStyles.cs
+++ Editor/ShadeStyles.cs
@@ -7,8 +7,8 @@ namespace HumToon.Editor
     public static class ShadeStyles
     {
         public static readonly GUIContent ShadeFoldout = EditorGUIUtility.TrTextContent("Shade", String.Empty);
-        public static readonly GUIContent FirstShadeMap = EditorGUIUtility.TrTextContent("First Shade Map", String.Empty);
-        public static readonly GUIContent FirstShadeBorderPos = EditorGUIUtility.TrTextContent("First Shade Border Pos", String.Empty);
-        public static readonly GUIContent FirstShadeBorderBlur = EditorGUIUtility.TrTextContent("First Shade Border Blur", String.Empty);
+        public static readonly GUIContent FirstShadeMap = EditorGUIUtility.TrTextContent("First Shade Map", "_FirstShadeColor & _FirstShadeMap");
+        public static readonly GUIContent FirstShadeBorderPos = EditorGUIUtility.TrTextContent("First Shade Border Pos", "_FirstShadeBorderPos");
+        public static readonly GUIContent FirstShadeBorderBlur = EditorGUIUtility.TrTextContent("First Shade Border Blur", "_FirstShadeBorderBlur");
     }
 }
