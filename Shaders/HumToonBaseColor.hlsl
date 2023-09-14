8ae3a5b98dc1998d694e01992b461e0a5fe4dbac
Hum9183 <hum9183@gmail.com>
2023-09-14 21:43:35 +0900

    リファクタ

--- Shaders/HumToonBaseColor.hlsl
+++ Shaders/HumToonBaseColor.hlsl
@@ -5,8 +5,8 @@
 
 half4 CalcBaseColor(float2 uv)
 {
-    half4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, uv);
-    half4 baseColor = baseMap * _BaseColor;
+    half4 baseMapColor = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, uv);
+    half4 baseColor = baseMapColor * _BaseColor;
 
     baseColor.a = AlphaDiscard(baseColor.a, _Cutoff);
     baseColor.rgb = AlphaModulate(baseColor.rgb, baseColor.a);
