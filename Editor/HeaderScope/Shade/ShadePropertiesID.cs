ea24a1ee7e7e405656dc75d3b7657896abe93bc0
Hum9183 <hum9183@gmail.com>
2023-09-23 16:17:23 +0900

    ShadeのValidatorをリファクタ

---
+++ Editor/HeaderScope/Shade/ShadePropertiesID.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using P = HumToon.Editor.ShadePropertiesContainer;
+
+namespace HumToon.Editor
+{
+    public static class ShadePropertiesID
+    {
+        public static readonly int ShadeMode          = ToID(nameof(P.ShadeMode));
+        public static readonly int UseFirstShade      = ToID(nameof(P.UseFirstShade));
+        public static readonly int FirstShadeMap      = ToID(nameof(P.FirstShadeMap));
+        public static readonly int UseExFirstShade    = ToID(nameof(P.UseExFirstShade));
+        public static readonly int UseSecondShade     = ToID(nameof(P.UseSecondShade));
+        public static readonly int SecondShadeMap     = ToID(nameof(P.SecondShadeMap));
+        public static readonly int UseShadeControlMap = ToID(nameof(P.UseShadeControlMap));
+        public static readonly int UseRampShade       = ToID(nameof(P.UseRampShade));
+        public static readonly int RampShadeMap       = ToID(nameof(P.RampShadeMap));
+        public static readonly int ShadeControlMap    = ToID(nameof(P.ShadeControlMap));
+
+        private static int ToID(string propNameWithoutPrefix)
+        {
+            return Shader.PropertyToID(propNameWithoutPrefix.Prefix());
+        }
+    }
+
+}
--- Editor/HeaderScope/Shade/ShadeValidator.cs
+++ Editor/HeaderScope/Shade/ShadeValidator.cs
@@ -1,23 +1,10 @@
-using System;
 using UnityEngine;
-using UnityEngine.Rendering;
 using P = HumToon.Editor.ShadePropertiesContainer;
 
 namespace HumToon.Editor
 {
     public class ShadeValidator : IHeaderScopeValidator
     {
-        private static readonly int IDShadeMode = Shader.PropertyToID($"{nameof(P.ShadeMode).Prefix()}");
-        private static readonly int IDUseFirstShade = Shader.PropertyToID($"{nameof(P.UseFirstShade).Prefix()}");
-        private static readonly int IDFirstShadeMap = Shader.PropertyToID($"{nameof(P.FirstShadeMap).Prefix()}");
-        private static readonly int IDUseExFirstShade = Shader.PropertyToID($"{nameof(P.UseExFirstShade).Prefix()}");
-        private static readonly int IDUseSecondShade = Shader.PropertyToID($"{nameof(P.UseSecondShade).Prefix()}");
-        private static readonly int IDSecondShadeMap = Shader.PropertyToID($"{nameof(P.SecondShadeMap).Prefix()}");
-        private static readonly int IDUseShadeControlMap = Shader.PropertyToID($"{nameof(P.UseShadeControlMap).Prefix()}");
-        private static readonly int IDUseRampShade = Shader.PropertyToID($"{nameof(P.UseRampShade).Prefix()}");
-        private static readonly int IDRampShadeMap = Shader.PropertyToID($"{nameof(P.RampShadeMap).Prefix()}");
-        private static readonly int IDShadeControlMap = Shader.PropertyToID($"{nameof(P.ShadeControlMap).Prefix()}");
-
         public void Validate(Material material)
         {
             SetKeywords(material);
@@ -25,54 +12,9 @@ namespace HumToon.Editor
 
         private void SetKeywords(Material material)
         {
-            // TODO:
-            // Modeを切り替えても、Texがアサインされていると_HUM_USE_FIRST_SHADE_MAPなどが定義されてしまう。
-            // やり方を考える。
-
-            ShadeMode shadeMode = material.GetFloatEnum<ShadeMode>(IDShadeMode);
-            (bool posAndBlur, bool ramp) = (false, false);
-            switch (shadeMode)
-            {
-                case ShadeMode.PosAndBlur:
-                    posAndBlur = true;
-                    break;
-                case ShadeMode.Ramp:
-                    ramp = true;
-                    break;
-                default:
-                    throw new NotImplementedException(nameof(shadeMode));
-            }
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_SHADE_MODE_POS_AND_BLUR, posAndBlur);
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_SHADE_MODE_RAMP, ramp);
-
-            // Pos And Blur
-
-            bool useFirstShade = material.GetFloat(IDUseFirstShade).ToBool();
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_USE_FIRST_SHADE, useFirstShade);
-
-            bool existsFirstShadeMap = material.GetTexture(IDFirstShadeMap) is not null;
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_USE_FIRST_SHADE_MAP, existsFirstShadeMap);
-
-            bool useExFirstShade = material.GetFloat(IDUseExFirstShade).ToBool();
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_USE_EX_FIRST_SHADE, useExFirstShade);
-
-            bool useSecondShade = material.GetFloat(IDUseSecondShade).ToBool();
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_USE_SECOND_SHADE, useSecondShade);
-
-            bool existsSecondShadeMap = material.GetTexture(IDSecondShadeMap) is not null;
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_USE_SECOND_SHADE_MAP, existsSecondShadeMap);
-
-            // Ramp
-
-            bool useRampShade = material.GetFloat(IDUseRampShade).ToBool();
-            bool existsRampShadeMap = material.GetTexture(IDRampShadeMap) is not null;
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_USE_RAMP_SHADE, useRampShade && existsRampShadeMap);
-
-            // Control Map
-
-            bool useShadeControlMap = material.GetFloat(IDUseShadeControlMap).ToBool();
-            bool existsShadeControlMap = material.GetTexture(IDShadeControlMap) is not null;
-            CoreUtils.SetKeyword(material, ShadeKeywordNames._HUM_USE_SHADE_CONTROL_MAP, useShadeControlMap && existsShadeControlMap);
+            var shadeKeywords = new ShadeKeywords();
+            shadeKeywords.Setup(material);
+            shadeKeywords.SetToMaterial(material);
         }
     }
 }
