54f791fb45c46cce6fd56d64656773bbea73eecf
Hum9183 <hum9183@gmail.com>
2023-09-23 10:02:22 +0900

    リファクタ

--- Editor/HeaderScope/Shade/ShadeKeywords.cs
+++ Editor/HeaderScope/Shade/ShadeKeywords.cs
@@ -2,11 +2,11 @@ namespace HumToon.Editor
 {
     public static class ShadeKeywords
     {
-        public static readonly string _HUM_USE_FIRST_SHADE      = "_HUM_USE_FIRST_SHADE";
-        public static readonly string _HUM_USE_FIRST_SHADE_MAP  = "_HUM_USE_FIRST_SHADE_MAP";
-        public static readonly string _HUM_USE_EX_FIRST_SHADE   = "_HUM_USE_EX_FIRST_SHADE";
-        public static readonly string _HUM_USE_SECOND_SHADE     = "_HUM_USE_SECOND_SHADE";
-        public static readonly string _HUM_USE_SECOND_SHADE_MAP = "_HUM_USE_SECOND_SHADE_MAP";
+        public static readonly string _HUM_USE_FIRST_SHADE       = "_HUM_USE_FIRST_SHADE";
+        public static readonly string _HUM_USE_FIRST_SHADE_MAP   = "_HUM_USE_FIRST_SHADE_MAP";
+        public static readonly string _HUM_USE_EX_FIRST_SHADE    = "_HUM_USE_EX_FIRST_SHADE";
+        public static readonly string _HUM_USE_SECOND_SHADE      = "_HUM_USE_SECOND_SHADE";
+        public static readonly string _HUM_USE_SECOND_SHADE_MAP  = "_HUM_USE_SECOND_SHADE_MAP";
         public static readonly string _HUM_USE_SHADE_CONTROL_MAP = "_HUM_USE_SHADE_CONTROL_MAP";
     }
 }
