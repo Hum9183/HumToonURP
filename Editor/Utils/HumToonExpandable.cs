e6b9f670011df87abbbd39b90099927329cdfe76
Hum9183 <hum9183@gmail.com>
2023-09-23 17:48:54 +0900

    HumToonEnumsをリネーム

---
+++ Editor/Utils/HumToonExpandable.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace HumToon.Editor
+{
+    /// <summary>
+    /// Flags for the foldouts used in the Hum Toon GUI.
+    /// </summary>
+    [Flags]
+    public enum Expandable
+    {
+        SurfaceOptions = 1 << 0,
+        Base = 1 << 1,
+        Normal = 1 << 2,
+        Shade = 1 << 3,
+        RimLight = 1 << 4,
+        Emission = 1 << 5,
+        MatCap = 1 << 6,
+        Light = 1 << 7,
+    }
+}
