0d8e43b2e2b807ae1865d369ff1a7fe0d85c153a
Hum9183 <hum9183@gmail.com>
2023-09-28 21:33:14 +0900

    MatCapのGUIを整理

--- Editor/HeaderScopes/MatCap/MatCapDrawer.cs
+++ Editor/HeaderScopes/MatCap/MatCapDrawer.cs
@@ -13,20 +13,27 @@ namespace Hum.HumToon.Editor.HeaderScopes.MatCap
 
         protected override void DrawInternal(MaterialEditor materialEditor)
         {
-            // TODO: Toggleの真横にIntensityのSliderがあるほうが良いかもしれない
             bool useMarCap = HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.UseMatCap, MatCapStyles.UseMatCap);
             if (useMarCap)
             {
                 using (new EditorGUI.IndentLevelScope())
                 {
-                    materialEditor.TexturePropertySingleLine(MatCapStyles.MatCapMap, PropContainer.MatCapMap, PropContainer.MatCapColor);
-                    materialEditor.TextureScaleOffsetProperty(PropContainer.MatCapMap);
+                    DrawMatCapMap(materialEditor);
                     materialEditor.ShaderProperty(PropContainer.MatCapIntensity, MatCapStyles.MatCapIntensity);
-                    materialEditor.ShaderProperty(PropContainer.MatCapMapMipLevel, MatCapStyles.MatCapMapMipLevel);
-                    materialEditor.ShaderProperty(PropContainer.MatCapMainLightEffectiveness, MatCapStyles.MatCapMainLightEffectiveness);
                     HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.MatCapCorrectPerspectiveDistortion, MatCapStyles.MatCapCorrectPerspectiveDistortion);
                     HumToonGUIUtils.DrawFloatToggleProperty(PropContainer.MatCapStabilizeCameraZRotation, MatCapStyles.MatCapStabilizeCameraZRotation);
+                    materialEditor.ShaderProperty(PropContainer.MatCapMainLightEffectiveness, MatCapStyles.MatCapMainLightEffectiveness);
                 }
+            }
+        }
+
+        private void DrawMatCapMap(MaterialEditor materialEditor)
+        {
+            materialEditor.TexturePropertySingleLine(MatCapStyles.MatCapMap, PropContainer.MatCapMap, PropContainer.MatCapColor);
+            using (new EditorGUI.IndentLevelScope())
+            {
+                materialEditor.TextureScaleOffsetProperty(PropContainer.MatCapMap);
+                materialEditor.ShaderProperty(PropContainer.MatCapMapMipLevel, MatCapStyles.MatCapMapMipLevel);
             }
         }
     }
