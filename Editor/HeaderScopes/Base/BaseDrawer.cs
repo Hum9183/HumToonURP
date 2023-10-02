98557fb7dd71672bbfc7c7c51ee5dad47b31c533
Hum9183 <hum9183@gmail.com>
2023-10-02 23:23:44 +0900

    リファクタ

--- Editor/HeaderScopes/Base/BaseDrawer.cs
+++ Editor/HeaderScopes/Base/BaseDrawer.cs
@@ -13,17 +13,7 @@ namespace Hum.HumToon.Editor.HeaderScopes.Base
 
         protected override void DrawInternal(MaterialEditor materialEditor)
         {
-            DrawBaseMap(materialEditor);
-            DrawTileOffset(materialEditor);
-        }
-
-        private void DrawBaseMap(MaterialEditor materialEditor)
-        {
             materialEditor.TexturePropertySingleLine(BaseStyles.BaseMap, PropContainer.BaseMap, PropContainer.BaseColor);
-        }
-
-        private void DrawTileOffset(MaterialEditor materialEditor)
-        {
             materialEditor.TextureScaleOffsetProperty(PropContainer.BaseMap);
         }
     }
