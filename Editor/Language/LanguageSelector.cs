2bc1f44377d92a2f8815cc8f4843e4dc7ac063c2
Hum9183 <hum9183@gmail.com>
2023-10-05 21:53:15 +0900

    Select、本処理を分割

--- Editor/Language/LanguageSelector.cs
+++ Editor/Language/LanguageSelector.cs
@@ -6,15 +6,20 @@ namespace Hum.HumToon.Editor.Language
     {
         public static string Select(string[] texts)
         {
+            return SelectInternal(texts, HumToonLanguage.DefaultLang, HumToonLanguage.CurrentLang);
+        }
+
+        private static string SelectInternal(string[] texts, Language defaultLang, Language currentLang)
+        {
             string result = string.Empty;
 
-            if (texts.TryGetValue((int)HumToonLanguage.DefaultLang, out string defaultLangText))
+            if (texts.TryGetValue((int)defaultLang, out string defaultLangText))
             {
                 if (string.IsNullOrEmpty(defaultLangText) is false)
                     result = defaultLangText;
             }
 
-            if (texts.TryGetValue((int)HumToonLanguage.CurrentLang, out string currentLangText))
+            if (texts.TryGetValue((int)currentLang, out string currentLangText))
             {
                 if (string.IsNullOrEmpty(currentLangText) is false)
                     result = currentLangText;
