3ca9da4ed37eaef8926e77c9c2cf9533c739fa43
Hum9183 <hum9183@gmail.com>
2023-10-11 21:15:28 +0900

    リファクタ

--- Editor/Utils/PassNames.cs
+++ Editor/Utils/PassNames.cs
@@ -2,7 +2,7 @@ namespace Hum.HumToon.Editor.Utils
 {
     public static class PassNames
     {
-        public static readonly string ShadowCaster = "ShadowCaster";
-        public static readonly string DepthOnly = "DepthOnly";
+        public static readonly string ShadowCaster = nameof(ShadowCaster);
+        public static readonly string DepthOnly = nameof(DepthOnly);
     }
 }
