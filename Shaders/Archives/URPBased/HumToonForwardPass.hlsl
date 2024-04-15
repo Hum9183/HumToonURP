f2a8d8683ebff8c008596083efaee1f09992c0d3
Hum9183 <hum9183@gmail.com>
2024-04-15 21:53:25 +0900

    Archivesシェーダを削除(コンパイルエラーが出るため)

--- Shaders/Archives/URPBased/HumToonForwardPass.hlsl
+++
@@ -1,9 +0,0 @@
-#ifndef HUM_TOON_FORWARD_PASS_INCLUDED
-#define HUM_TOON_FORWARD_PASS_INCLUDED
-
-#include "HumToonAttributes.hlsl"
-#include "HumToonVaryings.hlsl"
-#include "HumToonVert.hlsl"
-#include "HumToonFrag.hlsl"
-
-#endif
