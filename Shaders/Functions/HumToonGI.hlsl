161752814f1c1aa10278ef8f805ae6a9c8b3dc68
Hum9183 <hum9183@gmail.com>
2024-07-29 14:33:45 +0900

    リファクタ

--- Shaders/Functions/HumToonGI.hlsl
+++ Shaders/Functions/HumToonGI.hlsl
@@ -5,12 +5,12 @@ half3 HumGlobalIllumination(
     BRDFData brdfData, half3 bakedGI, half occlusion, float3 positionWS,
     half3 normalWS, half3 viewDirectionWS, float2 normalizedScreenSpaceUV)
 {
-    half3 reflectVector = reflect(-viewDirectionWS, normalWS);
+    half3 reflectionVector = reflect(-viewDirectionWS, normalWS);
     half NoV = saturate(dot(normalWS, viewDirectionWS));
     half fresnelTerm = Pow4(1.0 - NoV);
 
     half3 indirectDiffuse = bakedGI;
-    half3 indirectSpecular = GlossyEnvironmentReflection(reflectVector, positionWS, brdfData.perceptualRoughness, 1.0h, normalizedScreenSpaceUV);
+    half3 indirectSpecular = GlossyEnvironmentReflection(reflectionVector, positionWS, brdfData.perceptualRoughness, 1.0h, normalizedScreenSpaceUV);
 
     half3 giColor = EnvironmentBRDF(brdfData, indirectDiffuse, indirectSpecular, fresnelTerm);
 
